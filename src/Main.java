b3fffeeeda4808bec94f715b49544fc9b226f7fc
berkyd <berky.daniel@petrik.hu>
2022-10-06 14:13:14 +0200

    commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    /*
+    int nemStatikusAdattag;
+    static int statikusTag;
+    */
+
+    public static void main(String[] args) {
+        /*
+        Main objektum = new Main();
+        System.out.println(objektum.nemStatikusAdattag);
+        System.out.println(Main.statikusTag);
+        Math math = new Math();
+        */
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Veletlen.velEgesz(5, 10));
+        }
+        System.out.println();
+        for (int i = 0; i < 50; i++) {
+            System.out.println(Veletlen.velKarakter('A', 'Z'));
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Veletlen.velTeljesNev(true));
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Veletlen.velTeljesNev(false));
+        }
+    }
+}
---
+++ src/Veletlen.java
@@ -0,0 +1,103 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public final class Veletlen {
+    private Veletlen() {
+    }
+
+    private static final Random rnd = new Random();
+    private static final List<String> vezNevek = feltolt("files/veznev.txt");
+    private static final List<String> ferfiKerNevek = feltolt("files/ferfikernev.txt");
+    private static final List<String> noiKerNevek = feltolt("files/noikernev.txt");
+
+    private static List<String> feltolt(String fajlnev) {
+        List<String> lista = new ArrayList<>();
+        try {
+            Scanner file = new Scanner(new File(fajlnev));
+            while (file.hasNext()) {
+                String sor = file.nextLine();
+                lista.add(sor);
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public static int velEgesz(int min, int max) {
+        return rnd.nextInt(max - min + 1) + min;
+    }
+
+    public static char velKarakter(char min, char max) {
+        return (char) velEgesz(min, max);
+    }
+
+    public static String velVezetekNev() {
+        return vezNevek.get(rnd.nextInt(vezNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar keresztnév generálása
+     * @param nem A generált keresztnév neme. Férfi esetén true, Nő esetén false.
+     * @return A generált keresztnév
+     */
+    public static String velKeresztNev(boolean nem) {
+        String keresztNev;
+        if (nem) {
+            keresztNev = velFerfiKeresztNev();
+        } else {
+            keresztNev = velNoiKeresztNev();
+        }
+        return keresztNev;
+    }
+
+    private static String velFerfiKeresztNev() {
+        return ferfiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    private static String velNoiKeresztNev() {
+        return noiKerNevek.get(rnd.nextInt(noiKerNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar név generálása
+     * @param nem A generált név neme. Férfi esetén true, Nő esetén false.
+     * @return A generált név
+     */
+    public static String velTeljesNev(boolean nem) {
+        return velVezetekNev() + " " + velKeresztNev(nem);
+    }
+
+    public static String velDatum(){
+        int ev1;
+        int ev2;
+        return velDatum();
+    }
+
+    public static String velEmail(){
+        String nev;
+        return velEmail();
+    }
+
+    public static String velMobil(String nev){
+        String tszam = "";
+        for (int i = 0; i < 7 ; i++)
+        {
+            tszam += rnd.nextInt();
+        }
+        return "+36 (30) " + tszam;
+    }
+
+    public static String velSportag(){
+        return "";
+    }
+
+    public static String velSportegyesulet(){
+        return "";
+    }
+}
